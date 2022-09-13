a553ff0516c66e7542dae9505b2996769fdab0ef
Vladislav Mashin <mashinvladislav@gmail.com>
2022-09-13 22:16:15 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+def read_cookbook():
+    file_path = os.path.join(os.getcwd(), 'recipes.rtf')
+    cook_book = {}
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            dish_name = line.strip()
+            count = int(f.readline())
+            ing_list = list()
+            for item in range(count):
+                ingrs = {}
+                ingr = f.readline().strip()
+                ingrs['ingredient_name'], ingrs['quantity'], ingrs['measure'] = ingr.split('|')
+                ingrs['quantity'] = int(ingrs['quantity'])
+                ing_list.append(ingrs)
+            f.readline()
+            cook_book[dish_name] = ing_list
+    return cook_book
