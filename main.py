4693c81357088a3c35383e627a493bd19756541b
Vladislav Mashin <mashinvladislav@gmail.com>
2022-09-14 22:26:46 +0300

    Please enter the commit message for your changes. Lines starting
     Changes to be committed:

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
-
+import time
+from pprint import pprint
 def read_cookbook():
-    file_path = os.path.join(os.getcwd(), 'recipes.rtf')
+    file_path = os.path.join(os.getcwd(), 'recipes.txt')
     cook_book = {}
     with open(file_path, 'r', encoding='utf-8') as f:
         for line in f:
@@ -17,3 +18,30 @@ def read_cookbook():
             f.readline()
             cook_book[dish_name] = ing_list
     return cook_book
+def get_shop_list_by_dishes(dishes, person_count):
+    ingr_list = dict()
+
+    for dish_name in dishes:
+        if dish_name in cook_book:
+            for ings in cook_book[dish_name]:
+                meas_quan_list = dict()
+                if ings['ingredient_name'] not in ingr_list:
+                    meas_quan_list['measure'] = ings['measure']
+                    meas_quan_list['quantity'] = ings['quantity'] * person_count
+                    ingr_list[ings['ingredient_name']] = meas_quan_list
+                else:
+                    ingr_list[ings['ingredient_name']]['quantity'] = ingr_list[ings['ingredient_name']]['quantity'] + \
+                                                                     ings['quantity'] * person_count
+
+        else:
+            print(f'\n"Такого блюда нет в списке!"\n')
+    return ingr_list
+
+if __name__ == '__main__':
+    filename = "recipes.txt"
+    cook_book = read_cookbook()
+    print('Задание 1------------------------------------------------------------')
+    time.sleep(1)
+    print(cook_book)
+    print('Задание 2------------------------------------------------------------')
+    pprint(get_shop_list_by_dishes(dishes=['Запеченный картофель', 'Омлет'], person_count=2))
